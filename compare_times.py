52bd343dfcfc230fb1ebe562d767da0b5e5a5f7d
Russell Wallace <russell.wallace@gmail.com>
2020-11-10 06:48:26 +0000

    format

--- compare_times.py
+++ compare_times.py
@@ -2,16 +2,16 @@ import time
 import torch
 
 iterations = 10000
-size=1000
+size = 1000
 
 ########################################
 def f():
-    row=[0]*size
-    a=[]
+    row = [0] * size
+    a = []
     for i in range(size):
-        a.append([0]*size)
+        a.append([0] * size)
     for i in range(iterations):
-        a[10][10]=1-a[10][10]
+        a[10][10] = 1 - a[10][10]
 
 
 start = time.time()
@@ -20,13 +20,13 @@ print(f"{time.time()-start:12.6f}")
 
 ########################################
 def f():
-    row=[0]*size
-    a=[]
+    row = [0] * size
+    a = []
     for i in range(size):
-        a.append([0]*size)
-    a=torch.tensor(a)
+        a.append([0] * size)
+    a = torch.tensor(a)
     for i in range(iterations):
-        a[10][10]=1-a[10][10]
+        a[10][10] = 1 - a[10][10]
 
 
 start = time.time()
--- life.py
+++ life.py
@@ -1,49 +1,54 @@
 import random
 import torch
 
+
 def randboard():
-    size=10
-    b=[]
+    size = 10
+    b = []
     for i in range(size):
         b.append([random.randrange(2) for j in range(size)])
     return b
 
-def get(b,i,j):
-    size=len(b)
-    if i<0 or i>=size:
+
+def get(b, i, j):
+    size = len(b)
+    if i < 0 or i >= size:
         return 0
-    if j<0 or j>=size:
+    if j < 0 or j >= size:
         return 0
     return b[i][j]
 
+
 def step(b):
-    size=len(b)
-    b1=[]
+    size = len(b)
+    b1 = []
     for i in range(size):
-        row=[]
-        for    j  in range(size):
-            n=0
-            for i1 in range(i-1,i+2):
-                for j1 in range(j-1,j+2):
-                    if i1==i and j1==j:
+        row = []
+        for j in range(size):
+            n = 0
+            for i1 in range(i - 1, i + 2):
+                for j1 in range(j - 1, j + 2):
+                    if i1 == i and j1 == j:
                         continue
-                    n+=get(b,i1,j1)
+                    n += get(b, i1, j1)
             if b[i][j]:
-                c=n==2 or n==3
+                c = n == 2 or n == 3
             else:
-                c=n==3
+                c = n == 3
             row.append(int(c))
         b1.append(row)
     return b1
 
+
 def printboard(b):
     for row in b:
         for c in row:
-            print('@'if c else'.',end=' ')
+            print("@" if c else ".", end=" ")
         print()
     print()
 
-b=randboard()
+
+b = randboard()
 for i in range(5):
-    printboard (b)
-    b=step(b)
+    printboard(b)
+    b = step(b)
