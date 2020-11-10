c12ec5bcc7532e5b219fcbfcc2a1ea985605bb87
Russell Wallace <russell.wallace@gmail.com>
2020-11-10 06:47:50 +0000

    Conway's Game of life

--- compare_times.py
+++ compare_times.py
@@ -1,12 +1,17 @@
 import time
+import torch
 
-n = 10000000
+iterations = 10000
+size=1000
 
 ########################################
 def f():
-    s = [1, 2, 3]
-    for i in range(n):
-        tuple(map(lambda x: x + 1, s))
+    row=[0]*size
+    a=[]
+    for i in range(size):
+        a.append([0]*size)
+    for i in range(iterations):
+        a[10][10]=1-a[10][10]
 
 
 start = time.time()
@@ -15,12 +20,13 @@ print(f"{time.time()-start:12.6f}")
 
 ########################################
 def f():
-    s = [1, 2, 3]
-    for i in range(n):
-        r = []
-        for x in s:
-            r.append(x + 1)
-        tuple(r)
+    row=[0]*size
+    a=[]
+    for i in range(size):
+        a.append([0]*size)
+    a=torch.tensor(a)
+    for i in range(iterations):
+        a[10][10]=1-a[10][10]
 
 
 start = time.time()
