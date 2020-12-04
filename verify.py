b6f1dc63a090ff0afc5b0219207134afe1f04c0a
Russell Wallace <russell.wallace@gmail.com>
2020-12-04 08:08:53 +0000

    verify

---
+++ verify.py
@@ -0,0 +1,65 @@
+from prover import *
+
+
+def do_file(filename):
+    global attempted
+    global solved
+
+    # list file
+    if os.path.splitext(filename)[1] == ".lst":
+        for s in open(filename).readlines():
+            do_file(s.strip())
+        return
+
+    # try to solve
+    start = time.time()
+    set_timeout()
+    fname = os.path.basename(filename)
+    print(f"% {filename}")
+    try:
+        problem = read_problem(filename)
+        if problem.formulas:
+            print(f"% {len(problem.formulas)} formulas")
+        print(f"% {len(problem.clauses)} clauses")
+        r, conclusion = solve(problem.clauses)
+        if hasattr(problem, "conjecture"):
+            if r == "Satisfiable":
+                r = "CounterSatisfiable"
+            elif r == "Unsatisfiable":
+                r = "Theorem"
+        print(f"% SZS status {r} for {fname}")
+        if conclusion:
+            for c in conclusion.proof():
+                prformula(c)
+        if r in (
+            "Theorem",
+            "Unsatisfiable",
+            "ContradictoryAxioms",
+            "Satisfiable",
+            "CounterSatisfiable",
+        ):
+            if problem.expected and r != problem.expected:
+                if problem.expected == "ContradictoryAxioms" and r in (
+                    "Theorem",
+                    "Unsatisfiable",
+                ):
+                    pass
+                else:
+                    print(f"{r} != {problem.expected}")
+    except (Inappropriate, RecursionError, Timeout) as e:
+        print(f"% SZS status {e} for {fname}")
+    print(f"% {time.time() - start:.3f} seconds")
+    print()
+    for c in conclusion.proof():
+        if c.parents:
+            sys.stdout = open("1.p", "w")
+            for d in c.parents:
+                d.role = "axiom"
+                prformula(d)
+            c.role = "conjecture"
+            prformula(c)
+            sys.stdout.close()
+            break
+
+
+do_file(sys.argv[1])
