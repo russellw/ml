e9ddfdd21502cfd803a80c0c73abf8117fbb08d2
Russell Wallace <russell.wallace@gmail.com>
2020-12-08 15:25:14 +0000

    nn

--- test_dl/src/main/java/test_dl/Main.java
+++ test_dl/src/main/java/test_dl/Main.java
@@ -1,13 +1,24 @@
 package test_dl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.common.io.ClassPathResource;
+import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class Main {
   private static final int FEATURES_COUNT = 2;
@@ -22,6 +33,36 @@ public class Main {
           new RecordReaderDataSetIterator(recordReader, 150, FEATURES_COUNT, CLASSES_COUNT);
       DataSet allData = iterator.next();
       allData.shuffle(42);
+
+      DataNormalization normalizer = new NormalizerStandardize();
+      normalizer.fit(allData);
+      normalizer.transform(allData);
+
+      SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);
+      DataSet trainingData = testAndTrain.getTrain();
+      DataSet testData = testAndTrain.getTest();
+
+      MultiLayerConfiguration configuration =
+          new NeuralNetConfiguration.Builder()
+              // .iterations(1000)
+              .activation(Activation.TANH)
+              .weightInit(WeightInit.XAVIER)
+              // .learningRate(0.1)
+              .regularization(new ArrayList<>())
+              .l2(0.0001)
+              .list()
+              .layer(0, new DenseLayer.Builder().nIn(FEATURES_COUNT).nOut(3).build())
+              .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+              .layer(
+                  2,
+                  new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                      .activation(Activation.SOFTMAX)
+                      .nIn(3)
+                      .nOut(CLASSES_COUNT)
+                      .build())
+              .backprop(true)
+              .pretrain(false)
+              .build();
     }
   }
 }
