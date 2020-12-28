ffbc5cff6a84f2623886b21b218c13b319faa7ff
Russell Wallace <russell.wallace@gmail.com>
2020-12-28 02:45:58 +0000

    stats

--- tptp_stats.py
+++ tptp_stats.py
@@ -9,13 +9,13 @@ class Problem:
 problems = {}
 
 # e-cnf
-e = open("e-cnf.log").readlines()[2:]
+e = open("e-cnf1.log").readlines()[2:]
 e = [s.rstrip() for s in e]
 i = 0
 while 1:
     if i == len(e):
         break
-    m = re.match(r"TPTP/Problems/.../(.*-.*)\.p", e[i])
+    m = re.match(r"/mnt/c/ml/TPTP/Problems/.../(.*-.*)\.p", e[i])
     if not m:
         i += 1
         continue
@@ -27,7 +27,7 @@ while 1:
         p.timeout = 1
         i += 1
         continue
-    while i < len(e) and not e[i].startswith("TPTP"):
+    while i < len(e) and not e[i].startswith("/mnt/c/ml/TPTP"):
         m = re.match(r"# SZS status (\w+)", e[i])
         if m:
             p.szs = m[1]
@@ -48,6 +48,16 @@ while 1:
             p.e_formula_steps = int(m[1])
             i += 1
             continue
+        m = re.match(r"# Proof object given clauses           : (\d+)", e[i])
+        if m:
+            p.e_gcgood = int(m[1])
+            i += 1
+            continue
+        m = re.match(r"# Proof search given clauses           : (\d+)", e[i])
+        if m:
+            p.e_gctot = int(m[1])
+            i += 1
+            continue
         if (
             e[i] == "# SZS output start Saturation"
             or e[i] == "# SZS output start CNFRefutation"
@@ -64,6 +74,7 @@ while 1:
             p.e_proof = proof
             continue
         i += 1
+
 
 # s-cnf
 e = open("s-cnf.log").readlines()[2:]
@@ -86,6 +97,19 @@ while 1:
     i += 1
     p.s_proof = proof
 
+e = open("s-gc.log").readlines()[0:]
+e = [s.rstrip() for s in e]
+i = 0
+while 1:
+    if i == len(e):
+        break
+    m = e[i].split(",")
+    name = m[0][:-2]
+    i += 1
+    p = problems[name]
+    p.s_gcgood = int(m[1])
+    p.s_gctot = int(m[2])
+
 
 def p_key(p):
     return p.e_total_steps
@@ -104,6 +128,7 @@ for p in ps:
         print(dir(p))
         raise ValueError(p.name)
 ps = [p for p in ps if hasattr(p, "e_clause_steps")]
+ps = [p for p in ps if hasattr(p, "s_gcgood")]
 for p in ps:
     if p.e_total_steps != p.e_clause_steps + p.e_formula_steps:
         print(dir(p))
@@ -118,6 +143,18 @@ def writecsv(ps, filename):
     f = open(filename, "w")
     f.write("name")
     f.write(",")
+    f.write("e_gcgood")
+    f.write(",")
+    f.write("e_gctot")
+    f.write(",")
+    f.write("e_gcratio")
+    f.write(",")
+    f.write("s_gcgood")
+    f.write(",")
+    f.write("s_gctot")
+    f.write(",")
+    f.write("s_gcratio")
+    f.write(",")
     f.write("e_formula_steps")
     f.write(",")
     f.write("e_clause_steps")
@@ -128,6 +165,18 @@ def writecsv(ps, filename):
     f.write("\n")
     for p in ps:
         f.write(p.name)
+        f.write(",")
+        f.write(str(p.e_gcgood))
+        f.write(",")
+        f.write(str(p.e_gctot))
+        f.write(",")
+        f.write(str(p.e_gcgood / p.e_gctot))
+        f.write(",")
+        f.write(str(p.s_gcgood))
+        f.write(",")
+        f.write(str(p.s_gctot))
+        f.write(",")
+        f.write(str(p.s_gcgood / p.s_gctot))
         f.write(",")
         f.write(str(p.e_formula_steps))
         f.write(",")
