ffe8979f08120c58edaa32f2c23f32885f8230c0
Russell Wallace <russell.wallace@gmail.com>
2020-12-30 12:20:05 +0000

    refactor

--- clause_space/src/main/java/prover/TptpParser.java
+++ clause_space/src/main/java/prover/TptpParser.java
@@ -25,146 +25,6 @@ public final class TptpParser {
   private String tokString;
   private Map<String, Variable> free = new HashMap<>();
 
-  private TptpParser(String file, Set<String> select) throws IOException {
-    this.file = file;
-    reader =
-        new LineNumberReader(
-            new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-    reader.setLineNumber(1);
-    c = reader.read();
-    lex();
-    while (tok != -1) {
-      var s = word();
-      expect('(');
-      var name = word();
-      switch (s) {
-        case "cnf":
-          {
-            expect(',');
-
-            // Role
-            word();
-            expect(',');
-
-            // Formula
-            free.clear();
-            var negative = new ArrayList<Term>();
-            var positive = new ArrayList<Term>();
-            var parens = eat('(');
-            do {
-              var not = eat('~');
-              var a = infixUnary();
-              if (a instanceof Not) {
-                a = ((Not) a).a;
-                not = !not;
-              }
-              (not ? negative : positive).add(a);
-            } while (eat('|'));
-            if (parens) expect(')');
-            if ((select != null) && !select.contains(name)) break;
-            clauses.add(new Clause(negative, positive));
-            break;
-          }
-        case "include":
-          {
-            var tptp = System.getenv("TPTP");
-            if (tptp == null) throw new IllegalStateException("TPTP environment variable not set");
-            var select1 = select;
-            if (eat(','))
-              if ((tok == WORD) && "all".equals(tokString)) lex();
-              else {
-                expect('[');
-                select1 = new HashSet<>();
-                do {
-                  var name1 = word();
-                  if ((select == null) || select.contains(name1)) select1.add(name1);
-                } while (eat(','));
-                expect(']');
-              }
-            new TptpParser(tptp + '/' + name, select1);
-            break;
-          }
-        default:
-          throw new ParseException(file, reader.getLineNumber(), "unknown language");
-      }
-      expect(')');
-      expect('.');
-    }
-  }
-
-  public static List<Clause> read(String file) throws IOException {
-    clauses = new ArrayList<>();
-    functions = new HashMap<>();
-    new TptpParser(file, null);
-    return clauses;
-  }
-
-  private void args(List<Term> r) throws IOException {
-    expect('(');
-    do r.add(atomicTerm());
-    while (eat(','));
-    expect(')');
-  }
-
-  private Term atomicTerm() throws IOException {
-    var k = tok;
-    var s = tokString;
-    lex();
-    switch (k) {
-      case VAR:
-        {
-          var a = free.get(s);
-          if (a != null) return a;
-          a = new Variable();
-          free.put(s, a);
-          return a;
-        }
-      case WORD:
-        {
-          var a = functions.get(s);
-          if (a == null) {
-            a = new Function(s);
-            functions.put(s, a);
-          }
-          if (tok == '(') {
-            var r = new ArrayList<Term>();
-            args(r);
-            return a.call(r);
-          }
-          return a;
-        }
-      default:
-        throw new ParseException(file, reader.getLineNumber(), ": term expected");
-    }
-  }
-
-  private boolean eat(int k) throws IOException {
-    if (tok == k) {
-      lex();
-      return true;
-    }
-    return false;
-  }
-
-  private void expect(int k) throws IOException {
-    if (!eat(k))
-      throw new ParseException(file, reader.getLineNumber(), ": '" + (char) k + "' expected");
-  }
-
-  private Term infixUnary() throws IOException {
-    var a = atomicTerm();
-    switch (tok) {
-      case '=':
-        lex();
-        return new Equation(a, atomicTerm());
-      case NOT_EQ:
-        lex();
-        return new Not(new Equation(a, atomicTerm()));
-      default:
-        return a;
-    }
-  }
-
   private void lex() throws IOException {
     for (; ; ) {
       tok = c;
@@ -238,20 +98,22 @@ public final class TptpParser {
             break;
           }
         case '\'':
-          var line = reader.getLineNumber();
-          var quote = c;
-          var sb1 = new StringBuilder();
-          c = reader.read();
-          while (c != quote) {
-            if (c < ' ') throw new ParseException(file, line, "unclosed quote");
-            if (c == '\\') c = reader.read();
-            sb1.append((char) c);
+          {
+            var line = reader.getLineNumber();
+            var quote = c;
+            var sb = new StringBuilder();
             c = reader.read();
+            while (c != quote) {
+              if (c < ' ') throw new ParseException(file, line, "unclosed quote");
+              if (c == '\\') c = reader.read();
+              sb.append((char) c);
+              c = reader.read();
+            }
+            c = reader.read();
+            tokString = sb.toString();
+            tok = WORD;
+            break;
           }
-          c = reader.read();
-          tokString = sb1.toString();
-          tok = WORD;
-          break;
         case 'a':
         case 'b':
         case 'c':
@@ -296,11 +158,151 @@ public final class TptpParser {
     }
   }
 
+  private boolean eat(int k) throws IOException {
+    if (tok == k) {
+      lex();
+      return true;
+    }
+    return false;
+  }
+
+  private void expect(int k) throws IOException {
+    if (!eat(k))
+      throw new ParseException(file, reader.getLineNumber(), ": '" + (char) k + "' expected");
+  }
+
+  private void args(List<Term> r) throws IOException {
+    expect('(');
+    do r.add(atomicTerm());
+    while (eat(','));
+    expect(')');
+  }
+
+  private Term atomicTerm() throws IOException {
+    var k = tok;
+    var s = tokString;
+    lex();
+    switch (k) {
+      case VAR:
+        {
+          var a = free.get(s);
+          if (a != null) return a;
+          a = new Variable();
+          free.put(s, a);
+          return a;
+        }
+      case WORD:
+        {
+          var a = functions.get(s);
+          if (a == null) {
+            a = new Function(s);
+            functions.put(s, a);
+          }
+          if (tok == '(') {
+            var r = new ArrayList<Term>();
+            args(r);
+            return a.call(r);
+          }
+          return a;
+        }
+      default:
+        throw new ParseException(file, reader.getLineNumber(), ": term expected");
+    }
+  }
+
+  private Term infixUnary() throws IOException {
+    var a = atomicTerm();
+    switch (tok) {
+      case '=':
+        lex();
+        return new Equation(a, atomicTerm());
+      case NOT_EQ:
+        lex();
+        return new Not(new Equation(a, atomicTerm()));
+      default:
+        return a;
+    }
+  }
+
   private String word() throws IOException {
     if (tok != WORD) throw new ParseException(file, reader.getLineNumber(), "word expected");
     var s = tokString;
     lex();
     return s;
+  }
+
+  private TptpParser(String file, Set<String> select) throws IOException {
+    this.file = file;
+    reader =
+        new LineNumberReader(
+            new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+    reader.setLineNumber(1);
+    c = reader.read();
+    lex();
+    while (tok != -1) {
+      var s = word();
+      expect('(');
+      var name = word();
+      switch (s) {
+        case "cnf":
+          {
+            expect(',');
+
+            // Role
+            word();
+            expect(',');
+
+            // Formula
+            free.clear();
+            var negative = new ArrayList<Term>();
+            var positive = new ArrayList<Term>();
+            var parens = eat('(');
+            do {
+              var not = eat('~');
+              var a = infixUnary();
+              if (a instanceof Not) {
+                a = ((Not) a).a;
+                not = !not;
+              }
+              (not ? negative : positive).add(a);
+            } while (eat('|'));
+            if (parens) expect(')');
+            if ((select != null) && !select.contains(name)) break;
+            clauses.add(new Clause(negative, positive));
+            break;
+          }
+        case "include":
+          {
+            var tptp = System.getenv("TPTP");
+            if (tptp == null) throw new IllegalStateException("TPTP environment variable not set");
+            var select1 = select;
+            if (eat(','))
+              if ((tok == WORD) && "all".equals(tokString)) lex();
+              else {
+                expect('[');
+                select1 = new HashSet<>();
+                do {
+                  var name1 = word();
+                  if ((select == null) || select.contains(name1)) select1.add(name1);
+                } while (eat(','));
+                expect(']');
+              }
+            new TptpParser(tptp + '/' + name, select1);
+            break;
+          }
+        default:
+          throw new ParseException(file, reader.getLineNumber(), "unknown language");
+      }
+      expect(')');
+      expect('.');
+    }
+  }
+
+  public static List<Clause> read(String file) throws IOException {
+    clauses = new ArrayList<>();
+    functions = new HashMap<>();
+    new TptpParser(file, null);
+    return clauses;
   }
 
   private static final class Not extends Term {
