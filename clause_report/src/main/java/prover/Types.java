65b9037c6d671b56587bef5e85c14db907fd9460
Russell Wallace <russell.wallace@gmail.com>
2021-01-05 04:51:43 +0000

    symbols

--- clause_report/src/main/java/prover/Types.java
+++ clause_report/src/main/java/prover/Types.java
@@ -1,6 +1,7 @@
 package prover;
 
 import io.vavr.collection.Seq;
+import java.math.BigInteger;
 
 public final class Types {
   private Types() {}
@@ -12,9 +13,28 @@ public final class Types {
     if (a instanceof Symbol)
       switch ((Symbol) a) {
         case EQUALS:
+        case AND:
+        case OR:
+        case EQV:
+        case NOT:
+        case IS_INTEGER:
+        case IS_RATIONAL:
+        case EXISTS:
+        case ALL:
+        case LESS:
+        case LESS_EQ:
           return Symbol.BOOLEAN;
+        case TO_INTEGER:
+          return Symbol.INTEGER;
+        case TO_REAL:
+          return Symbol.REAL;
+        case TO_RATIONAL:
+          return Symbol.RATIONAL;
       }
     if (a instanceof Boolean) return Symbol.BOOLEAN;
+    if (a instanceof BigInteger) return Symbol.INTEGER;
+    if (a instanceof BigRational) return Symbol.RATIONAL;
+    if (a instanceof String) return Symbol.INDIVIDUAL;
     throw new IllegalArgumentException(a + ": " + a.getClass());
   }
 }
