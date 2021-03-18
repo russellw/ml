cdbd268dba2de11ead1d0219bfd4dc69fb9de959
Russell Wallace <russell.wallace@gmail.com>
2021-03-18 15:24:44 +0000

    tests

--- mpir_test/build-mpir.bat
+++ mpir_test/build-mpir.bat
@@ -1,5 +1,5 @@
 pushd \mpir\msvc\vs19
-call msbuild.bat gc DLL x64 Debug
-call msbuild.bat gc LIB x64 Release
+call msbuild.bat gc DLL x64 Debug +tests
+call msbuild.bat gc LIB x64 Release +tests
 popd
 copy \mpir\dll\x64\Debug\mpir.dll
