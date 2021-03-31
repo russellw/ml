2a532997d2e9ae1107c638c896f162818001bd16
Russell Wallace <russell.wallace@gmail.com>
2021-03-31 17:19:01 +0100

    clean

--- search/bench.bat
+++
@@ -1,4 +0,0 @@
-if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /DBENCHMARK /EHsc /Feayane /I\mpir /J /O2 *.cc \mpir\release.lib
-if errorlevel 1 goto :eof
-ayane
