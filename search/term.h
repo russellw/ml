5367ed370e6568ded4cd0ab96c886031b2fd0d7f
Russell Wallace <russell.wallace@gmail.com>
2021-04-01 00:14:13 +0100

    test

--- search/term.h
+++ search/term.h
@@ -10,7 +10,7 @@ enum {
 #define term(p, t) ((si)(p) + (t))
 #define tag(a) ((a)&7)
 
-static Int *intptr(si a) {
+static Int *intp(si a) {
   assert(tag(a) == t_int);
   return (Int *)(a - t_int);
 }
--- search/test.c
+++ search/test.c
@@ -3,5 +3,25 @@
 #include "main.h"
 
 #ifdef DEBUG
-void test(void) { assert(internz("abc") == internz("abc")); }
+static si mkint(si val) {
+  Int x;
+  mpz_init_set_si(x.val, val);
+  Int *p = intern_int(&x);
+  assert(mpz_get_si(p->val) == val);
+  Int y;
+  mpz_init_set_si(y.val, val);
+  assert(intern_int(&y) == p);
+  si r = term(p, t_int);
+  assert(tag(r) == t_int);
+  assert(intp(r) == p);
+  return r;
+}
+
+void test(void) {
+  assert(internz("abc") == internz("abc"));
+  si zero = mkint(0);
+  si one = mkint(1);
+  si two = mkint(2);
+  si three = mkint(3);
+}
 #endif
