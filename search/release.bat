7a2c292a08ab2e923d4710c77c22691aaff7f430
Russell Wallace <russell.wallace@gmail.com>
2021-03-31 17:25:25 +0100

    C

--- search/release.bat
+++ search/release.bat
@@ -1,4 +1,4 @@
 if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 move *.asm \temp
-cl /EHsc /Fa /Feayane /I\mpir /J /O2 *.cc \mpir\release.lib
+cl /Fa /Fea /Ilib /I\mpir /J /O2 *.c \mpir\release.lib
 wc *.asm
