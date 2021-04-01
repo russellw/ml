2d5db93c220f7217dc96a1c5770f11aaee1923c5
Russell Wallace <russell.wallace@gmail.com>
2021-04-01 01:08:57 +0100

    refactor

--- search/release.bat
+++ search/release.bat
@@ -1,4 +1,4 @@
 if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 move *.asm \temp
-cl /Fa /Fea /Ilib /I\mpir /J /O2 *.c \mpir\release.lib
+cl /Fa /Fea /Ilib /I\mpir /J /O2 *.c lib\*.c \mpir\release.lib
 wc *.asm
--- search/vtun.bat
+++ search/vtun.bat
@@ -1,4 +1,4 @@
 if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 if [%~1]==[] goto :eof
-cl /EHsc /Feayane /I\mpir /J /MTd /O2 /Zi *.cc \mpir\debug.lib dbghelp.lib
-"C:\Program Files (x86)\Intel\oneAPI\vtune\latest\bin64\vtune" -collect hotspots -user-data-dir \temp ayane.exe %*
+cl /Fea /Ilib /I\mpir /J /MTd /O2 /Zi *.c lib\*.c \mpir\debug.lib dbghelp.lib
+"C:\Program Files (x86)\Intel\oneAPI\vtune\latest\bin64\vtune" -collect hotspots -user-data-dir \temp a.exe %*
