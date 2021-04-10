763cb022632394beb02170f1a29f2dfe9be47d0b
Russell Wallace <russell.wallace@gmail.com>
2021-04-10 19:51:24 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 'use strict'
 var unify = require('./unify')
+
+function help(){
+	console.log('Options:')
+	console.log()
+	console.log('-h  Show help')
+	console.log('-v  Show version')
+}
+
+function version(){
+	console.log('Version 0')
+}
+
+var files=[]
+for(var arg of process.argv.slice(2)){
+	var s=arg
+	if(!s.startsWith('-')){
+		files.push(s)
+		continue
+	}
+	while(s.startsWith('-'))
+	s=s.slice(1)
+	switch(s){
+		case'h':
+		case'help':
+		help();
+		continue
+		case'h':
+		case'version':
+		version();
+		continue
+	}
+	console.log('%s: unknown option',arg)
+	process.exit(1)
+}
+console.log(files)
