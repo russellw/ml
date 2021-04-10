b0709bfb9e8d3d0d34360af9f78ec1adc4e9e395
Russell Wallace <russell.wallace@gmail.com>
2021-04-10 20:05:58 +0100

    prettier

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
-'use strict'
-var unify = require('./unify')
+"use strict";
+var unify = require("./unify");
 
-function help(){
-	console.log('Options:')
-	console.log()
-	console.log('-h  Show help')
-	console.log('-v  Show version')
+function help() {
+  console.log("Options:");
+  console.log();
+  console.log("-h  Show help");
+  console.log("-v  Show version");
 }
 
-function version(){
-	console.log('Version 0')
+function version() {
+  console.log("Version 0");
 }
 
-var files=[]
-for(var arg of process.argv.slice(2)){
-	var s=arg
-	if(!s.startsWith('-')){
-		files.push(s)
-		continue
-	}
-	while(s.startsWith('-'))
-	s=s.slice(1)
-	switch(s){
-		case'h':
-		case'help':
-		help();
-		continue
-		case'h':
-		case'version':
-		version();
-		continue
-	}
-	console.log('%s: unknown option',arg)
-	process.exit(1)
+var files = [];
+for (var arg of process.argv.slice(2)) {
+  var s = arg;
+  if (!s.startsWith("-")) {
+    files.push(s);
+    continue;
+  }
+  while (s.startsWith("-")) s = s.slice(1);
+  switch (s) {
+    case "h":
+    case "help":
+      help();
+      continue;
+    case "h":
+    case "version":
+      version();
+      continue;
+  }
+  console.log("%s: unknown option", arg);
+  process.exit(1);
 }
-console.log(files)
+console.log(files);
