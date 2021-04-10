63f78b29f420713e461b5dcc3035040d715a1552
Russell Wallace <russell.wallace@gmail.com>
2021-04-10 20:21:36 +0100

    tabs

--- js/main.js
+++ js/main.js
@@ -2,35 +2,35 @@
 var unify = require('./unify')
 
 function help() {
-    console.log('Options:')
-    console.log()
-    console.log('-h  Show help')
-    console.log('-v  Show version')
+	console.log('Options:')
+	console.log()
+	console.log('-h  Show help')
+	console.log('-v  Show version')
 }
 
 function version() {
-    console.log('Version 0')
+	console.log('Version 0')
 }
 
 var files = []
 for (var arg of process.argv.slice(2)) {
-    var s = arg
-    if (!s.startsWith('-')) {
-        files.push(s)
-        continue
-    }
-    while (s.startsWith('-')) s = s.slice(1)
-    switch (s) {
-        case 'h':
-        case 'help':
-            help()
-            continue
-        case 'h':
-        case 'version':
-            version()
-            continue
-    }
-    console.log('%s: unknown option', arg)
-    process.exit(1)
+	var s = arg
+	if (!s.startsWith('-')) {
+		files.push(s)
+		continue
+	}
+	while (s.startsWith('-')) s = s.slice(1)
+	switch (s) {
+		case 'h':
+		case 'help':
+			help()
+			continue
+		case 'h':
+		case 'version':
+			version()
+			continue
+	}
+	console.log('%s: unknown option', arg)
+	process.exit(1)
 }
 console.log(files)
