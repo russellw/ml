614bee9a19241499a495585552c4deec01a22c45
Russell Wallace <russell.wallace@gmail.com>
2021-04-11 03:56:44 +0100

    fmt

--- js/logic.js
+++ js/logic.js
@@ -68,27 +68,27 @@ function fn(name) {
 
 function term(op, ...args) {
 	switch (op) {
-	case '!=':
-	case '<':
-	case '<=':
-	case '<=>':
-	case '<~>':
-	case '=':
-	case '=>':
-	case '>':
-	case '>=':
-	case '~&':
-	case '~|':
-		assert(args.length === 2)
-		break
-	case '&':
-	case '|':
-		break
-	case '~':
-		assert(args.length === 1)
-		break
-	default:
-		throw new Error(op)
+		case '!=':
+		case '<':
+		case '<=':
+		case '<=>':
+		case '<~>':
+		case '=':
+		case '=>':
+		case '>':
+		case '>=':
+		case '~&':
+		case '~|':
+			assert(args.length === 2)
+			break
+		case '&':
+		case '|':
+			break
+		case '~':
+			assert(args.length === 1)
+			break
+		default:
+			throw new Error(op)
 	}
 	var a = Array.from(args)
 	a.op = op
@@ -104,10 +104,10 @@ function variable(name) {
 
 function integer(val) {
 	switch (typeof val) {
-	case 'number':
-	case 'string':
-		val = BigInt(val)
-		break
+		case 'number':
+		case 'string':
+			val = BigInt(val)
+			break
 	}
 	var a = []
 	a.op = 'integer'
@@ -116,56 +116,69 @@ function integer(val) {
 }
 
 function isTerm(a) {
-	if (!Array.isArray(a))
-		return
-	if (typeof a.op !== 'string')
-		return
+	if (!Array.isArray(a)) return
+	if (typeof a.op !== 'string') return
 	return true
 }
 
 function eq(a, b) {
 	assert(isTerm(a))
 	assert(isTerm(b))
-	if (a === b)
-		return true
-	if (a.op !== b.op)
-		return
-	if (a.length !== b.length)
-		return
-	for (var i = 0; i < a.length; i++)
-		if (!eq(a[i], b[i]))
-			return
+	if (a === b) return true
+	if (a.op !== b.op) return
+	if (a.length !== b.length) return
+	for (var i = 0; i < a.length; i++) if (!eq(a[i], b[i])) return
 	switch (a.op) {
-	case 'integer':
-	case 'bool':
-		return a.val === b.val
-	case 'call':
-		return a.f === b.f
-	case 'distinctObj':
-	return a.name===b.name
-	case 'fn':
-	case 'variable':
-		return
+		case 'integer':
+		case 'bool':
+			return a.val === b.val
+		case 'call':
+			return a.f === b.f
+		case 'distinctObj':
+			return a.name === b.name
+		case 'fn':
+		case 'variable':
+			return
 	}
 	return true
 }
 
 //bool
-assert(eq(bool(false),bool(false)))
-assert(eq(bool(true),bool(true)))
-assert(!eq(bool(false),bool(true)))
+assert(eq(bool(false), bool(false)))
+assert(eq(bool(true), bool(true)))
+assert(!eq(bool(false), bool(true)))
 
 //integer
-assert(eq(integer(0),integer(0)))
-assert(!eq(integer(0),integer(1)))
-assert(eq(integer(1_000_000_000_000_000_000_000_000n),integer(1_000_000_000_000n*1_000_000_000_000n)))
-assert(eq(integer(1_000_000_000_000_000_000_000_000n),integer("1000000000000000000000000")))
-assert(eq(integer(1_000_000_000_000_000_000_000_000n),integer("+1000000000000000000000000")))
-assert(eq(integer(-1_000_000_000_000_000_000_000_000n),integer("-1000000000000000000000000")))
+assert(eq(integer(0), integer(0)))
+assert(!eq(integer(0), integer(1)))
+assert(
+	eq(
+		integer(1_000_000_000_000_000_000_000_000n),
+		integer(1_000_000_000_000n * 1_000_000_000_000n)
+	)
+)
+assert(
+	eq(
+		integer(1_000_000_000_000_000_000_000_000n),
+		integer('1000000000000000000000000')
+	)
+)
+assert(
+	eq(
+		integer(1_000_000_000_000_000_000_000_000n),
+		integer('+1000000000000000000000000')
+	)
+)
+assert(
+	eq(
+		integer(-1_000_000_000_000_000_000_000_000n),
+		integer('-1000000000000000000000000')
+	)
+)
 
 //distinct object
-assert(eq(distinctObj('a'),distinctObj('a')))
-assert(!eq(distinctObj('a'),distinctObj('b')))
+assert(eq(distinctObj('a'), distinctObj('a')))
+assert(!eq(distinctObj('a'), distinctObj('b')))
 
 exports.occurs = occurs
 exports.distinctObj = distinctObj
