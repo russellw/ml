4fca2b6d78f1b9b26c05744a8203b54d468478d7
Russell Wallace <russell.wallace@gmail.com>
2021-04-11 08:25:31 +0100

    transform

---
+++ scrap.js
@@ -0,0 +1,19 @@
+
+function transform(a,f){
+	if(!a.length)return f(a)
+	var r=[]
+	Object.assign(r,a)
+	for(var i=0;i<r.length;i++)
+	r[i]=transform(r[i],f)
+	return r
+}
+
+//transform
+function t1(a){
+	if(a.op=='integer')return integer(a.val+BigInt(1))
+	return a
+}
+
+assert(eq(transform(a,t1), a))
+assert(eq(transform(term('&&', bool(true), bool(true)),t1), term('&&', bool(true), bool(true))))
+assert(eq(transform(call(f, [integer(1), integer(2)]),t1), call(f, [integer(2), integer(3)])))
