a8bea878fcf146e5642cd9db114eed2a2cfaebac
Russell Wallace <russell.wallace@gmail.com>
2021-04-18 12:11:48 +0100

    tests

--- js/subsumption.js
+++ js/subsumption.js
@@ -82,172 +82,174 @@ function subsumes(c, d) {
 	return match(c0, d0, c1, d1, new Map())
 }
 
-// test
-var a = { type: 'individual' }
-var a1 = { type: ['individual', 'individual'] }
-var b = { type: 'individual' }
-var p = { type: 'bool' }
-var p1 = { type: ['bool', 'individual'] }
-var p2 = { type: ['bool', 'individual', 'individual'] }
-var q = { type: 'bool' }
-var q1 = { type: ['bool', 'individual'] }
-var x = { o: 'var', name: 'x', type: 'individual' }
-var y = { o: 'var', name: 'y', type: 'individual' }
-var c, d
+function test() {
+	var a = { type: 'individual' }
+	var a1 = { type: ['individual', 'individual'] }
+	var b = { type: 'individual' }
+	var p = { type: 'bool' }
+	var p1 = { type: ['bool', 'individual'] }
+	var p2 = { type: ['bool', 'individual', 'individual'] }
+	var q = { type: 'bool' }
+	var q1 = { type: ['bool', 'individual'] }
+	var x = { o: 'var', name: 'x', type: 'individual' }
+	var y = { o: 'var', name: 'y', type: 'individual' }
+	var c, d
 
-// false <= false
-c = [[], []]
-d = [[], []]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// false <= false
+	c = [[], []]
+	d = [[], []]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// false <= p
-c = [[], []]
-d = [[], [p]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// false <= p
+	c = [[], []]
+	d = [[], [p]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p <= p
-c = [[], [p]]
-d = [[], [p]]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p <= p
+	c = [[], [p]]
+	d = [[], [p]]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// !p <= !p
-c = [[p], []]
-d = [[p], []]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// !p <= !p
+	c = [[p], []]
+	d = [[p], []]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p <= p | p
-c = [[], [p]]
-d = [[], [p, p]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// p <= p | p
+	c = [[], [p]]
+	d = [[], [p, p]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p !<= !p
-c = [[], [p]]
-d = [[p], []]
-assert(!subsumes(c, d))
-assert(!subsumes(d, c))
+	// p !<= !p
+	c = [[], [p]]
+	d = [[p], []]
+	assert(!subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p | q <= q | p
-c = [[], [p, q]]
-d = [[], [q, p]]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p | q <= q | p
+	c = [[], [p, q]]
+	d = [[], [q, p]]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p | q <= p | q | p
-c = [[], [p, q]]
-d = [[], [p, q, p]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// p | q <= p | q | p
+	c = [[], [p, q]]
+	d = [[], [p, q, p]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p(a) | p(b) | q(a) | q(b) | <= p(a) | q(a) | p(b) | q(b)
-c = [[], [etc.mk('call', p, a), etc.mk('call', p, b), etc.mk('call', q, a), etc.mk('call', q, b)]]
-d = [[], [etc.mk('call', p, a), etc.mk('call', q, a), etc.mk('call', p, b), etc.mk('call', q, b)]]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p(a) | p(b) | q(a) | q(b) | <= p(a) | q(a) | p(b) | q(b)
+	c = [[], [etc.mk('call', p, a), etc.mk('call', p, b), etc.mk('call', q, a), etc.mk('call', q, b)]]
+	d = [[], [etc.mk('call', p, a), etc.mk('call', q, a), etc.mk('call', p, b), etc.mk('call', q, b)]]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p(x,y) <= p(a,b)
-c = [[], [etc.mk('call', p2, x, y)]]
-d = [[], [etc.mk('call', p2, a, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// p(x,y) <= p(a,b)
+	c = [[], [etc.mk('call', p2, x, y)]]
+	d = [[], [etc.mk('call', p2, a, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p(x,x) !<= p(a,b)
-c = [[], [etc.mk('call', p2, x, x)]]
-d = [[], [etc.mk('call', p2, a, b)]]
-assert(!subsumes(c, d))
-assert(!subsumes(d, c))
+	// p(x,x) !<= p(a,b)
+	c = [[], [etc.mk('call', p2, x, x)]]
+	d = [[], [etc.mk('call', p2, a, b)]]
+	assert(!subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p(x) <= p(y)
-c = [[], [etc.mk('call', p1, x)]]
-d = [[], [etc.mk('call', p1, y)]]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p(x) <= p(y)
+	c = [[], [etc.mk('call', p1, x)]]
+	d = [[], [etc.mk('call', p1, y)]]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p(x) | p(a(x)) | p(a(a(x))) <= p(y) | p(a(y)) | p(a(a(y)))
-c = [
-	[],
-	[
-		etc.mk('call', p1, x),
-		etc.mk('call', p1, etc.mk('call', a1, x)),
-		etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, x))),
-	],
-]
-d = [
-	[],
-	[
-		etc.mk('call', p1, y),
-		etc.mk('call', p1, etc.mk('call', a1, y)),
-		etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, y))),
-	],
-]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p(x) | p(a(x)) | p(a(a(x))) <= p(y) | p(a(y)) | p(a(a(y)))
+	c = [
+		[],
+		[
+			etc.mk('call', p1, x),
+			etc.mk('call', p1, etc.mk('call', a1, x)),
+			etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, x))),
+		],
+	]
+	d = [
+		[],
+		[
+			etc.mk('call', p1, y),
+			etc.mk('call', p1, etc.mk('call', a1, y)),
+			etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, y))),
+		],
+	]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p(x) | p(a) <= p(a) | p(b)
-c = [[], [etc.mk('call', p1, x), etc.mk('call', p1, a)]]
-d = [[], [etc.mk('call', p1, a), etc.mk('call', p1, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// p(x) | p(a) <= p(a) | p(b)
+	c = [[], [etc.mk('call', p1, x), etc.mk('call', p1, a)]]
+	d = [[], [etc.mk('call', p1, a), etc.mk('call', p1, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p(x) | p(a(x)) <= p(a(y)) | p(y)
-c = [[], [etc.mk('call', p1, x), etc.mk('call', p1, etc.mk('call', a1, x))]]
-d = [[], [etc.mk('call', p1, etc.mk('call', a1, y)), etc.mk('call', p1, y)]]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p(x) | p(a(x)) <= p(a(y)) | p(y)
+	c = [[], [etc.mk('call', p1, x), etc.mk('call', p1, etc.mk('call', a1, x))]]
+	d = [[], [etc.mk('call', p1, etc.mk('call', a1, y)), etc.mk('call', p1, y)]]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// p(x) | p(a(x)) | p(a(a(x))) <= p(a(a(y))) | p(a(y)) | p(y)
-c = [
-	[],
-	[
-		etc.mk('call', p1, x),
-		etc.mk('call', p1, etc.mk('call', a1, x)),
-		etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, x))),
-	],
-]
-d = [
-	[],
-	[
-		etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, y))),
-		etc.mk('call', p1, etc.mk('call', a1, y)),
-		etc.mk('call', p1, y),
-	],
-]
-assert(subsumes(c, d))
-assert(subsumes(d, c))
+	// p(x) | p(a(x)) | p(a(a(x))) <= p(a(a(y))) | p(a(y)) | p(y)
+	c = [
+		[],
+		[
+			etc.mk('call', p1, x),
+			etc.mk('call', p1, etc.mk('call', a1, x)),
+			etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, x))),
+		],
+	]
+	d = [
+		[],
+		[
+			etc.mk('call', p1, etc.mk('call', a1, etc.mk('call', a1, y))),
+			etc.mk('call', p1, etc.mk('call', a1, y)),
+			etc.mk('call', p1, y),
+		],
+	]
+	assert(subsumes(c, d))
+	assert(subsumes(d, c))
 
-// (a = x) <= (a = b)
-c = [[], [etc.mk('==', a, x)]]
-d = [[], [etc.mk('==', a, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// (a = x) <= (a = b)
+	c = [[], [etc.mk('==', a, x)]]
+	d = [[], [etc.mk('==', a, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// (x = a) <= (a = b)
-c = [[], [etc.mk('==', x, a)]]
-d = [[], [etc.mk('==', a, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// (x = a) <= (a = b)
+	c = [[], [etc.mk('==', x, a)]]
+	d = [[], [etc.mk('==', a, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// !p(y) | !p(x) | q(x) <= !p(a) | !p(b) | q(b)
-c = [[etc.mk('call', p1, y), etc.mk('call', p1, x)], [etc.mk('call', q1, x)]]
-d = [[etc.mk('call', p1, a), etc.mk('call', p1, b)], [etc.mk('call', q1, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// !p(y) | !p(x) | q(x) <= !p(a) | !p(b) | q(b)
+	c = [[etc.mk('call', p1, y), etc.mk('call', p1, x)], [etc.mk('call', q1, x)]]
+	d = [[etc.mk('call', p1, a), etc.mk('call', p1, b)], [etc.mk('call', q1, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// !p(x) | !p(y) | q(x) <= !p(a) | !p(b) | q(b)
-c = [[etc.mk('call', p1, x), etc.mk('call', p1, y)], [etc.mk('call', q1, x)]]
-d = [[etc.mk('call', p1, a), etc.mk('call', p1, b)], [etc.mk('call', q1, b)]]
-assert(subsumes(c, d))
-assert(!subsumes(d, c))
+	// !p(x) | !p(y) | q(x) <= !p(a) | !p(b) | q(b)
+	c = [[etc.mk('call', p1, x), etc.mk('call', p1, y)], [etc.mk('call', q1, x)]]
+	d = [[etc.mk('call', p1, a), etc.mk('call', p1, b)], [etc.mk('call', q1, b)]]
+	assert(subsumes(c, d))
+	assert(!subsumes(d, c))
 
-// p(x,a(x)) !<= p(a(y),a(y))
-c = [[], [etc.mk('call', p2, x, etc.mk('call', a1, x))]]
-d = [[], [etc.mk('call', p2, etc.mk('call', a1, y), y)]]
-assert(!subsumes(c, d))
-assert(!subsumes(d, c))
+	// p(x,a(x)) !<= p(a(y),a(y))
+	c = [[], [etc.mk('call', p2, x, etc.mk('call', a1, x))]]
+	d = [[], [etc.mk('call', p2, etc.mk('call', a1, y), y)]]
+	assert(!subsumes(c, d))
+	assert(!subsumes(d, c))
+}
 
-// exports
+test()
+
 exports.subsumes = subsumes
