f9f44b13d11ab7d41680f02582a9a16a28b40ffd
Russell Wallace <russell.wallace@gmail.com>
2021-04-18 11:41:00 +0100

    tests

--- js/subsumption.js
+++ js/subsumption.js
@@ -31,7 +31,7 @@ function match(c0, d0, c1, d1, m) {
 			// same thing with the literals on the other side
 			var de = logic.eqn(d0[di])
 			var dx = d0.slice()
-			dx .splice(di, 1)
+			dx.splice(di, 1)
 
 			// try orienting equation one way
 			var m1 = new Map(m)
@@ -59,264 +59,264 @@ function match(c0, d0, c1, d1, m) {
 
 function subsumes(c, d) {
 	console.log('=============================================')
-	console.dir(c===d)
-	console.dir(c,{depth:null})
-	console.dir(d,{depth:null})
+	console.dir(c === d)
+	console.dir(c, { depth: null })
+	console.dir(d, { depth: null })
 	console.log(logic.freevars(c))
 	console.log(logic.freevars(d))
-	assert(c.length==2)
-	assert(d.length==2)
+	assert(c.length === 2)
+	assert(d.length === 2)
 
-	//clauses are assumed to have distinct variable names
-	for  (x of logic.freevars(c))assert(!logic.freevars(d).has(x))
+	// clauses are assumed to have distinct variable names
+	for (x of logic.freevars(c)) assert(!logic.freevars(d).has(x))
 
-	//negative and positive sides need to be matched separately
-	//though of course with shared variable assignments
-	var [c0,c1]=c
-	var [d0,d1]=d
+	// negative and positive sides need to be matched separately
+	// though of course with shared variable assignments
+	var [c0, c1] = c
+	var [d0, d1] = d
 
-	//it is impossible for a longer clause to subsume a shorter one
-	if(c0.length>d0.length||c1.length>d1.length)return
+	// it is impossible for a longer clause to subsume a shorter one
+	if (c0.length > d0.length || c1.length > d1.length) return
 
-    // fewer literals typically fail faster
-    	console.trace([c0,d0,c1,d1])
-    if (c1.length < c0.length) {
-    	[c1,c0]=c
-    	;[d1,d0]=d
-    	console.trace([c0,d0,c1,d1])
-    }
-    	console.trace([c0,d0,c1,d1])
-
-    //search for matched literals
-    return match(c0,d0,c1,d1,new Map())
+	// fewer literals typically fail faster
+	console.trace([c0, d0, c1, d1])
+	if (c1.length < c0.length) {
+		;[c1, c0] = c
+		;[d1, d0] = d
+		console.trace([c0, d0, c1, d1])
 	}
+	console.trace([c0, d0, c1, d1])
 
-//test
-    var a ={type:'individual'}
-    var a1 ={type:['individual','individual']}
-    var b ={type:'individual'}
-    var p ={type:'bool'}
-    var p1 ={type:['bool','individual']}
-    var p2 ={type:['bool','individual','individual']}
-    var q ={type:'bool'}
-    var q1 ={type:['bool','individual']}
-    var x ={o:'var',name:'x',type:'individual'}
-    var y ={o:'var',name:'y',type:'individual'}
-    var c,d
+	// search for matched literals
+	return match(c0, d0, c1, d1, new Map())
+}
 
-    // false <= false
-    assert(x.name!==y.name)
-    c = [[],[]]
-    ;d= [[],[]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// test
+var a = { type: 'individual' }
+var a1 = { type: ['individual', 'individual'] }
+var b = { type: 'individual' }
+var p = { type: 'bool' }
+var p1 = { type: ['bool', 'individual'] }
+var p2 = { type: ['bool', 'individual', 'individual'] }
+var q = { type: 'bool' }
+var q1 = { type: ['bool', 'individual'] }
+var x = { o: 'var', name: 'x', type: 'individual' }
+var y = { o: 'var', name: 'y', type: 'individual' }
+var c, d
 
-    // false <= p
-    assert(x.name!==y.name)
-    c = [[],[]]
-    ;d= [[],[p]]
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// false <= false
+assert(x.name !== y.name)
+c = [[], []]
+d = [[], []]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p <= p
-    assert(x.name!==y.name)
-    c = [[],[p]]
-    ;d= [[],[p]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// false <= p
+assert(x.name !== y.name)
+c = [[], []]
+d = [[], [p]]
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // !p <= !p
-    assert(x.name!==y.name)
-    c = [[p],[]]
-    ;d= [[p],[]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p <= p
+assert(x.name !== y.name)
+c = [[], [p]]
+d = [[], [p]]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p <= p | p
-    assert(x.name!==y.name)
-    c = [[],[p]]
-    ;d= [[],[p,p]]
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// !p <= !p
+assert(x.name !== y.name)
+c = [[p], []]
+d = [[p], []]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p !<= !p
-    assert(x.name!==y.name)
-    c = [[],[p]]
-    ;d= [[p],[]]
-    assert(!subsumes(c, d));
-    assert(!subsumes(d, c));
+// p <= p | p
+assert(x.name !== y.name)
+c = [[], [p]]
+d = [[], [p, p]]
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p | q <= q | p
-    assert(x.name!==y.name)
-    c = [[],[p,q]]
-    ;d= [[],[q,p]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p !<= !p
+assert(x.name !== y.name)
+c = [[], [p]]
+d = [[p], []]
+assert(!subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p | q <= p | q | p
-    assert(x.name!==y.name)
-    c = [[],[p,q]]
-    ;d= [[],[p,q,p]]
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// p | q <= q | p
+assert(x.name !== y.name)
+c = [[], [p, q]]
+d = [[], [q, p]]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p(a) | p(b) | q(a) | q(b) | <= p(a) | q(a) | p(b) | q(b)
-    assert(x.name!==y.name)
-    c = [[],[etc.mk('call',p,a),etc.mk('call',p,b),etc.mk('call',q,a),etc.mk('call',q,b)]]
-    ;d= [[],[etc.mk('call',p,a),etc.mk('call',q,a),etc.mk('call',p,b),etc.mk('call',q,b)]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p | q <= p | q | p
+assert(x.name !== y.name)
+c = [[], [p, q]]
+d = [[], [p, q, p]]
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p(x,y) <= p(a,b)
-    assert(x.name!==y.name)
-    c = [[],[etc.mk('call',p2,x,y)]]
-    ;d= [[],[etc.mk('call',p2,a,b)]]
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// p(a) | p(b) | q(a) | q(b) | <= p(a) | q(a) | p(b) | q(b)
+assert(x.name !== y.name)
+c = [[], [etc.mk('call', p, a), etc.mk('call', p, b), etc.mk('call', q, a), etc.mk('call', q, b)]]
+d = [[], [etc.mk('call', p, a), etc.mk('call', q, a), etc.mk('call', p, b), etc.mk('call', q, b)]]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p(x,x) !<= p(a,b)
-    assert(x.name!==y.name)
-    c = [[],[etc.mk('call',p2,x,x)]]
-    ;d= [[],[etc.mk('call',p2,a,b)]]
-    assert(!subsumes(c, d));
-    assert(!subsumes(d, c));
+// p(x,y) <= p(a,b)
+assert(x.name !== y.name)
+c = [[], [etc.mk('call', p2, x, y)]]
+d = [[], [etc.mk('call', p2, a, b)]]
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p(x) <= p(y)
-    console.dir(x)
-    console.dir(y)
-    assert(x.name!==y.name)
-     c = [[],[etc.mk('call',p1,x)]]
-     ;d= [[],[etc.mk('call',p1,y)]]
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p(x,x) !<= p(a,b)
+assert(x.name !== y.name)
+c = [[], [etc.mk('call', p2, x, x)]]
+d = [[], [etc.mk('call', p2, a, b)]]
+assert(!subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p(x) | p(a(x)) | p(a(a(x))) <= p(y) | p(a(y)) | p(a(a(y)))
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, x));
-    positive.add(List.of(p1, List.of(a1, x)));
-    positive.add(List.of(p1, List.of(a1, List.of(a1, x))));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, y));
-    positive.add(List.of(p1, List.of(a1, y)));
-    positive.add(List.of(p1, List.of(a1, List.of(a1, y))));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p(x) <= p(y)
+console.dir(x)
+console.dir(y)
+assert(x.name !== y.name)
+c = [[], [etc.mk('call', p1, x)]]
+d = [[], [etc.mk('call', p1, y)]]
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p(x) | p(a) <= p(a) | p(b)
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, x));
-    positive.add(List.of(p1, a));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, a));
-    positive.add(List.of(p1, b));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// p(x) | p(a(x)) | p(a(a(x))) <= p(y) | p(a(y)) | p(a(a(y)))
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, x))
+positive.add(List.of(p1, List.of(a1, x)))
+positive.add(List.of(p1, List.of(a1, List.of(a1, x))))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, y))
+positive.add(List.of(p1, List.of(a1, y)))
+positive.add(List.of(p1, List.of(a1, List.of(a1, y))))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // p(x) | p(a(x)) <= p(a(y)) | p(y)
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, x));
-    positive.add(List.of(p1, List.of(a1, x)));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, List.of(a1, y)));
-    positive.add(List.of(p1, y));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p(x) | p(a) <= p(a) | p(b)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, x))
+positive.add(List.of(p1, a))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, a))
+positive.add(List.of(p1, b))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p(x) | p(a(x)) | p(a(a(x))) <= p(a(a(y))) | p(a(y)) | p(y)
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, x));
-    positive.add(List.of(p1, List.of(a1, x)));
-    positive.add(List.of(p1, List.of(a1, List.of(a1, x))));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p1, List.of(a1, List.of(a1, y))));
-    positive.add(List.of(p1, List.of(a1, y)));
-    positive.add(List.of(p1, y));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(subsumes(d, c));
+// p(x) | p(a(x)) <= p(a(y)) | p(y)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, x))
+positive.add(List.of(p1, List.of(a1, x)))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, List.of(a1, y)))
+positive.add(List.of(p1, y))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // (a = x) <= (a = b)
-    negative.clear();
-    positive.clear();
-    positive.add(Equality.of(a, x));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(Equality.of(a, b));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// p(x) | p(a(x)) | p(a(a(x))) <= p(a(a(y))) | p(a(y)) | p(y)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, x))
+positive.add(List.of(p1, List.of(a1, x)))
+positive.add(List.of(p1, List.of(a1, List.of(a1, x))))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(List.of(p1, List.of(a1, List.of(a1, y))))
+positive.add(List.of(p1, List.of(a1, y)))
+positive.add(List.of(p1, y))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(subsumes(d, c))
 
-    // (x = a) <= (a = b)
-    negative.clear();
-    positive.clear();
-    positive.add(Equality.of(x, a));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(Equality.of(a, b));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// (a = x) <= (a = b)
+negative.clear()
+positive.clear()
+positive.add(Equality.of(a, x))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(Equality.of(a, b))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // !p(y) | !p(x) | q(x) <= !p(a) | !p(b) | q(b)
-    negative.clear();
-    negative.add(List.of(p1, y));
-    negative.add(List.of(p1, x));
-    positive.clear();
-    positive.add(List.of(q1, x));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    negative.add(List.of(p1, a));
-    negative.add(List.of(p1, b));
-    positive.clear();
-    positive.add(List.of(q1, b));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// (x = a) <= (a = b)
+negative.clear()
+positive.clear()
+positive.add(Equality.of(x, a))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(Equality.of(a, b))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // !p(x) | !p(y) | q(x) <= !p(a) | !p(b) | q(b)
-    negative.clear();
-    negative.add(List.of(p1, x));
-    negative.add(List.of(p1, y));
-    positive.clear();
-    positive.add(List.of(q1, x));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    negative.add(List.of(p1, a));
-    negative.add(List.of(p1, b));
-    positive.clear();
-    positive.add(List.of(q1, b));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(subsumes(c, d));
-    assert(!subsumes(d, c));
+// !p(y) | !p(x) | q(x) <= !p(a) | !p(b) | q(b)
+negative.clear()
+negative.add(List.of(p1, y))
+negative.add(List.of(p1, x))
+positive.clear()
+positive.add(List.of(q1, x))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+negative.add(List.of(p1, a))
+negative.add(List.of(p1, b))
+positive.clear()
+positive.add(List.of(q1, b))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-    // p(x,a(x)) !<= p(a(y),a(y))
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p2, x, List.of(a1, x)));
-    c = new Clause(negative, positive, Inference.AXIOM);
-    negative.clear();
-    positive.clear();
-    positive.add(List.of(p2, List.of(a1, y), List.of(a1, y)));
-    d = new Clause(negative, positive, Inference.AXIOM);
-    assert(!subsumes(c, d));
-    assert(!subsumes(d, c));
+// !p(x) | !p(y) | q(x) <= !p(a) | !p(b) | q(b)
+negative.clear()
+negative.add(List.of(p1, x))
+negative.add(List.of(p1, y))
+positive.clear()
+positive.add(List.of(q1, x))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+negative.add(List.of(p1, a))
+negative.add(List.of(p1, b))
+positive.clear()
+positive.add(List.of(q1, b))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(subsumes(c, d))
+assert(!subsumes(d, c))
 
-//exports
-exports.subsumes=subsumes
+// p(x,a(x)) !<= p(a(y),a(y))
+negative.clear()
+positive.clear()
+positive.add(List.of(p2, x, List.of(a1, x)))
+c = new Clause(negative, positive, Inference.AXIOM)
+negative.clear()
+positive.clear()
+positive.add(List.of(p2, List.of(a1, y), List.of(a1, y)))
+d = new Clause(negative, positive, Inference.AXIOM)
+assert(!subsumes(c, d))
+assert(!subsumes(d, c))
+
+// exports
+exports.subsumes = subsumes
