a973426347153e76526ab9c1cdc28374bb63cf2f
Russell Wallace <russell.wallace@gmail.com>
2021-04-27 23:00:59 +0100

    test

--- js/cdcl.js
+++ js/cdcl.js
@@ -96,6 +96,92 @@ function test() {
 		[[], [a]],
 	])
 	assert(!m)
+
+	function dpll(clauses, m, deadline) {
+		var cs = clauses.map((c) => cnf.simplify(c, m))
+		for (var c of cs) if (etc.eq(c, [[], []])) return
+		cs = cs.filter((c) => !etc.eq(c, [[], [true]]))
+		if (!cs.length) return m
+
+		// unit clauses
+		for (var c of cs) {
+			var [neg, pos] = c
+			if (neg.length + pos.length === 1) {
+				if (neg.length) m.set(neg[0], false)
+				else m.set(pos[0], true)
+				return dpll(clauses, m, deadline)
+			}
+		}
+
+		// atoms
+		var atoms = new Set()
+		for (var c of cs) for (var L of c) for (var a of L) atoms.add(a)
+
+		// pure atoms
+		function occurs(pol, a) {
+			for (var c of cs) if (c[pol].includes(a)) return true
+		}
+
+		for (var a of atoms)
+			if (occurs(0, a) !== occurs(1, a)) {
+				m.set(a, !!occurs(1, a))
+				return dpll(clauses, m, deadline)
+			}
+
+		// guess
+		for (var a of atoms) {
+			etc.cktime(deadline)
+			var m1 = new Map(m)
+			m1.set(a, false)
+			var r = dpll(clauses, m1, deadline)
+			if (r) return r
+			m.set(a, true)
+			return dpll(clauses, m, deadline)
+		}
+	}
+
+	var m = dpll([[[], []]], new Map())
+	assert(!m)
+
+	var a = {}
+	m = dpll([[[], [a]]], new Map())
+	assert(m)
+	assert(m.size === 1)
+	assert(m.get(a) === true)
+
+	var b = {}
+	m = dpll(
+		[
+			[[], [a]],
+			[[], [b]],
+		],
+		new Map()
+	)
+	assert(m)
+	assert(m.size === 2)
+	assert(m.get(a) === true)
+	assert(m.get(b) === true)
+
+	m = dpll(
+		[
+			[[a], []],
+			[[b], []],
+		],
+		new Map()
+	)
+	assert(m)
+	assert(m.size === 2)
+	assert(m.get(a) === false)
+	assert(m.get(b) === false)
+
+	m = dpll(
+		[
+			[[a], []],
+			[[], [a]],
+		],
+		new Map()
+	)
+	assert(!m)
 }
 
 test()
