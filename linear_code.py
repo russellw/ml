e6773902ae81b54d3708f047d46d0fc2fe05a10d
Russell Wallace <russell.wallace@gmail.com>
2022-07-02 21:26:26 +0100

    simple linear code representation

---
+++ linear_code.py
@@ -0,0 +1,36 @@
+import random
+
+ops = ("+", "-", "*", "/", "0", "1")
+
+
+def rand():
+    v = []
+    for i in range(10):
+        a = random.choice(ops)
+        v.append(a)
+    return v
+
+
+def run(code):
+    stack = []
+    for a in code:
+        if a.isdigit():
+            stack.append(int(a))
+        elif len(stack) < 2:
+            stack = [0]
+        else:
+            x = stack[-2]
+            y = stack[-1]
+            if a == "/" and y == 0:
+                z = 0
+            else:
+                z = eval(str(x) + a + str(y))
+            stack = stack[:-2] + [z]
+    return stack[-1]
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        code = rand()
+        print(code)
+        print(run(code))
