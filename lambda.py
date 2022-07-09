07d00d479aa6c05f591048c823c242c943f66de1
Russell Wallace <russell.wallace@gmail.com>
2022-07-09 23:29:25 +0100

    lambda

--- lambda.py
+++ lambda.py
@@ -1,29 +1,47 @@
-import random
 import operator
+import random
 
 atoms = (0, 1, "arg")
 ops = {
+    "*": operator.mul,
     "+": operator.add,
     "-": operator.sub,
-    "*": operator.mul,
     "/": operator.truediv,
     "<": operator.lt,
     "<=": operator.le,
     "=": operator.eq,
+    "div": operator.floordiv,
     "if": None,
     "lambda": None,
+    "mod": operator.mod,
+    "pow": operator.pow,
+    "at": lambda a, b: a[int(b)],
+    "cons": lambda a, b: [a] + b,
+    "hd": lambda a: a[0],
+    "len": lambda a: len(a),
+    "map": map,
+    "not": operator.not_,
+    "tl": lambda a: a[1:],
+}
+arity = {
+    "hd": 1,
+    "if": 3,
+    "lambda": 1,
+    "len": 1,
+    "not": 1,
+    "tl": 1,
 }
 
 
 def rand(depth):
+    if not random.randrange(0, 16):
+        depth = 0
     if depth:
         depth -= 1
         o = random.choice(list(ops.keys()))
         n = 2
-        if o == "if":
-            n = 3
-        elif o == "lambda":
-            n = 1
+        if o in arity:
+            n = arity[o]
         v = [o]
         for i in range(n):
             v.append(rand(depth))
@@ -41,14 +59,6 @@ class Closure:
 
     def __call__(self, arg):
         return eva(self.body, self.env + [arg])
-
-
-def apply(f, args):
-    x = args[0]
-    if type(f) is Closure:
-        return eva(f.body, f.env)
-    y = args[1]
-    return eval(str(x) + f + str(y))
 
 
 def eva(a, env):
@@ -73,10 +83,12 @@ def eva(a, env):
 
 
 if __name__ == "__main__":
-    for i in range(1000):
-        a = rand(3)
+    for i in range(10000000):
+        a = rand(4)
         try:
             x = eva(a, []) + 0
+            if x < 3:
+                continue
             print(a)
             print(x)
             print()
