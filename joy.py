22a6d70b7ae7d84dd06c19d97502205a9a2bc9f1
Russell Wallace <russell.wallace@gmail.com>
2022-07-12 03:16:37 +0100

    fold

--- joy.py
+++ joy.py
@@ -62,6 +62,35 @@ def le():
     stack.append(x <= y)
 
 
+# logic
+def not1():
+    x = stack.pop()
+    stack.append(not x)
+
+
+def and1():
+    # TODO: does this need short-circuit evaluation?
+    y = stack.pop()
+    x = stack.pop()
+    stack.append(x and y)
+
+
+def or1():
+    y = stack.pop()
+    x = stack.pop()
+    stack.append(x or y)
+
+
+def if1():
+    y = stack.pop()
+    x = stack.pop()
+    c = stack.pop()
+    if c:
+        run(x)
+    else:
+        run(y)
+
+
 # stack
 def dup():
     x = stack[-1]
@@ -79,6 +108,61 @@ def swap():
     stack.append(x)
 
 
+# lists
+def identity():
+    f = stack.pop()
+    run(f)
+
+
+def map1():
+    f = stack.pop()
+    v = stack.pop()
+    r = []
+    for x in v:
+        stack.append(x)
+        r.append(run1(f))
+    stack.append(r)
+
+
+def filter1():
+    f = stack.pop()
+    v = stack.pop()
+    r = []
+    for x in v:
+        stack.append(x)
+        if run1(f):
+            r.append(x)
+    stack.append(r)
+
+
+def fold():
+    f = stack.pop()
+    a = stack.pop()
+    v = stack.pop()
+    stack.append(a)
+    for x in v:
+        stack.append(x)
+        run(f)
+
+
+# recursion
+def linrec1(c, then, rec1, rec2):
+    if run1(c):
+        run(then)
+        return
+    run(rec1)
+    linrec1(c, then, rec1, rec2)
+    run(rec2)
+
+
+def linrec():
+    rec2 = stack.pop()
+    rec1 = stack.pop()
+    then = stack.pop()
+    c = stack.pop()
+    linrec1(c, then, rec1, rec2)
+
+
 ops = {
     # arithmetic
     "+": add,
@@ -91,10 +175,22 @@ ops = {
     "=": eq,
     "<": lt,
     "<=": le,
+    # logic
+    "not": not1,
+    "and": and1,
+    "or": or1,
+    "if": if1,
     # stack
     "dup": dup,
     "pop": pop,
     "swap": swap,
+    # lists
+    "i": identity,
+    "map": map1,
+    "filter": filter1,
+    "fold": fold,
+    # recursion
+    "linrec": linrec,
 }
 
 
@@ -161,28 +257,49 @@ def parse(v):
 
 # interpreter
 def run(code):
+    if type(code) != list:
+        stack.append(code)
+        return
     for a in code:
         if type(a) is str:
             ops[a]()
             continue
         stack.append(a)
-    return stack[-1]
+
+
+def run1(code):
+    run(code)
+    return stack.pop()
 
 
 def test(s, r):
+    global stack
+    stack = []
     v = lex(s)
     code = parse(v)
-    x = run(code)
+    print(code)
+    run(code)
+    print(stack)
+    x = stack[-1]
     assert x == r
+    print()
 
 
 if __name__ == "__main__":
     assert lex("3 dup *") == ["3", "dup", "*"]
     assert parse(lex("3 dup *")) == [3, "dup", "*"]
+
     test("3 dup *", 9)
-    code = [3, "dup", "*"]
-    x = run(code)
-    print(x)
+    test("[1 2 3 4] [dup *] map", [1, 4, 9, 16])
+    test("[1 2 3 4] [2 swap <] filter", [3, 4])
+    test("[2 5 3] 0 [+] fold", 10)
+    test("[2 5 3] 0 [dup * +] fold", 38)
+    test("[1 1 1 + +] i", 3)
+    test("1 2 3 if", 2)
+    test("1 [ 1 1 +] [1 1 1 + +] if", 2)
+    test("0 [ 1 1 +] [1 1 1 + +] if", 3)
+    test("4 not 1 [dup 1 -] * linrec", 24)
+
     exit(0)
     for i in range(20):
         code = rand(10)
