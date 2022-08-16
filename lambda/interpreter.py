b9e851e3eec147faf2bcf309c6fcc9b80e596f8b
Russell Wallace <russell.wallace@gmail.com>
2022-08-16 06:48:46 +0100

    refactor

--- lambda/interpreter.py
+++ lambda/interpreter.py
@@ -1,19 +1,5 @@
-import random
-
 from etc import *
 import types1
-
-
-def ev(env, a):
-    if isinstance(a, tuple):
-        if not a:
-            return a
-        o, *s = a
-        return evs[o](env, *s)
-    if isinstance(a, str):
-        return env.get(a)
-    return a
-
 
 t = Var()
 lst = ("list", t)
@@ -63,85 +49,15 @@ for name, _, f in ops:
     evs[name] = f
 
 
-def lam(env, t, depth):
-    params = []
-    paramts = []
-    for paramt in t[2:]:
-        params.append("a" + str(env.count()))
-        paramts.append(paramt)
-    env = Env(env, params, paramts)
-    body = rand(env, t[1], depth)
-    return "lambda", tuple(params), body
-
-
-def rand(env, t, depth):
-    t = freshVars(t)
-    s = []
-
-    # required or decided to return an atom
-    if not depth or not random.randrange(0, 16):
-        # available local variables that match the required type
-        for a in env.keys1():
-            if types1.unify({}, env.get(a), t):
-                s.append(a)
-
-        # some types can also be provided by literals
-        match t:
-            case "bool":
-                s.append(False)
-                s.append(True)
-            case "num":
-                s.append(0)
-                s.append(1)
-            case "fn", *_:
-                # if we were supposed to be returning an atom, prefer to avoid further recursion,
-                # but if the required return type is a function,
-                # and we don't have any variables of that function type to hand,
-                # then we don't have a choice
-                if not s:
-                    return lam(env, t, 0)
-            case "list", _:
-                s.append(())
-
-        # choose a suitable atom at random
-        return random.choice(s)
-
-    # one more level of compound recursion
-    depth -= 1
-
-    # operators that match the required type
-    for name, u, _ in ops:
-        if u and types1.unify({}, u[0], t):
-            s.append((name, u))
-    match t:
-        case "fn", *_:
-            s.append(("lambda", None))
-
-    # choose a suitable operator at random
-    name, u = random.choice(s)
-
-    # recursively generate arguments
-    if name == "lambda":
-        return lam(env, t, depth)
-
-    d = {}
-    types1.unify(d, u[0], t)
-    u = replace(d, u)
-
-    s = [name]
-    for t in u[1:]:
-        s.append(rand(env, t, depth))
-    return tuple(s)
-
-
-env = Env()
-env["a"] = "list", "bool"
-random.seed(1)
-for i in range(20):
-    a = rand(env, ("list", "bool"), 5)
-    print(a)
-    print(simplify(a))
-    print()
+def ev(env, a):
+    if isinstance(a, tuple):
+        if not a:
+            return a
+        o, *s = a
+        return evs[o](env, *s)
+    if isinstance(a, str):
+        return env.get(a)
+    return a
 
 
 def test(code, expected, arg=None):
@@ -209,15 +125,3 @@ if __name__ == "__main__":
     test(("map", square, s), (1, 4, 9))
 
     print("ok")
-    exit(0)
-    for i in range(10000000):
-        a = rand(4)
-        try:
-            x = ev(a, [])
-            if len(x) < 2:
-                continue
-            print(a)
-            print(x)
-            print()
-        except:
-            pass
---
+++ lambda/rand.py
@@ -0,0 +1,87 @@
+import random
+
+from etc import *
+import interpreter
+import types1
+
+
+def lam(env, t, depth):
+    params = []
+    paramts = []
+    for paramt in t[2:]:
+        params.append("a" + str(env.count()))
+        paramts.append(paramt)
+    env = Env(env, params, paramts)
+    body = rand(env, t[1], depth)
+    return "lambda", tuple(params), body
+
+
+def rand(env, t, depth):
+    t = freshVars(t)
+    s = []
+
+    # required or decided to return an atom
+    if not depth or not random.randrange(0, 16):
+        # available local variables that match the required type
+        for a in env.keys1():
+            if types1.unify({}, env.get(a), t):
+                s.append(a)
+
+        # some types can also be provided by literals
+        match t:
+            case "bool":
+                s.append(False)
+                s.append(True)
+            case "num":
+                s.append(0)
+                s.append(1)
+            case "fn", *_:
+                # if we were supposed to be returning an atom, prefer to avoid further recursion,
+                # but if the required return type is a function,
+                # and we don't have any variables of that function type to hand,
+                # then we don't have a choice
+                if not s:
+                    return lam(env, t, 0)
+            case "list", _:
+                s.append(())
+
+        # choose a suitable atom at random
+        return random.choice(s)
+
+    # one more level of compound recursion
+    depth -= 1
+
+    # operators that match the required type
+    for name, u, _ in interpreter.ops:
+        if u and types1.unify({}, u[0], t):
+            s.append((name, u))
+    match t:
+        case "fn", *_:
+            s.append(("lambda", None))
+
+    # choose a suitable operator at random
+    name, u = random.choice(s)
+
+    # recursively generate arguments
+    if name == "lambda":
+        return lam(env, t, depth)
+
+    d = {}
+    types1.unify(d, u[0], t)
+    u = replace(d, u)
+
+    s = [name]
+    for t in u[1:]:
+        s.append(rand(env, t, depth))
+    return tuple(s)
+
+
+if __name__ == "__main__":
+    env = Env()
+    env["a"] = "list", "bool"
+    random.seed(1)
+    for i in range(20):
+        a = rand(env, ("list", "bool"), 5)
+        print(a)
+        print(simplify(a))
+        print()
