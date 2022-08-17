e97b0e58d718b28c0b10363d2b8dad37510cdc88
Russell Wallace <russell.wallace@gmail.com>
2022-08-17 09:36:58 +0100

    untyped

--- lambda/etc.py
+++ lambda/etc.py
@@ -31,22 +31,9 @@ class Env(dict):
         return s
 
 
-class Var:
-    # this class is intended for logic variables, not necessarily program variables
-    def __init__(self, t=None):
-        self.t = t
-
-    def __repr__(self):
-        if not hasattr(self, "name"):
-            return "Var"
-        return self.name
-
-
 def const(a):
     match a:
         case str():
-            return
-        case Var():
             return
         case ():
             return True
@@ -62,62 +49,11 @@ def dbg(a):
     print(f"{info.filename}:{info.function}:{info.lineno}: {repr(a)}")
 
 
-def freeVars(a):
-    free = set()
-
-    def rec(bound, a):
-        match a:
-            case Var() as a:
-                if a not in bound:
-                    free.add(a)
-            case "lambda", params, body:
-                rec(bound | set(params), body)
-            case _, *s:
-                for a in s:
-                    rec(bound, a)
-
-    rec(set(), a)
-    return free
-
-
-def freshVars(a):
-    d = {}
-
-    def rec(a):
-        match a:
-            case Var() as a:
-                if a not in d:
-                    d[a] = Var(a.t)
-            case _, *s:
-                for a in s:
-                    rec(a)
-
-    rec(a)
-    return replace(d, a)
-
-
-def replace(d, a):
-    if a in d:
-        return replace(d, d[a])
-    if isinstance(a, tuple):
-        return tuple([replace(d, b) for b in a])
-    return a
-
-
 if __name__ == "__main__":
-    a = "a"
-    x = Var()
-
-    assert const(True)
     assert const(1)
-    assert not const(a)
-    assert not const(x)
+    assert not const("a")
     assert const(())
     assert const(("quote", "a"))
     assert not const(("not", "a"))
-
-    assert freeVars("a") == set()
-    assert freeVars(x) == set([x])
-    assert freeVars(("+", x, x)) == set([x])
 
     print("ok")
