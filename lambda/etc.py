72ffbc2fcc59b8025268d9619a4e569ec8f00006
Russell Wallace <russell.wallace@gmail.com>
2022-08-18 00:23:27 +0100

    fix

--- lambda/etc.py
+++ lambda/etc.py
@@ -1,36 +1,6 @@
 import inspect
 
 
-class Env(dict):
-    def __init__(self, outer=None, params=(), args=()):
-        self.outer = outer
-        self.update(zip(params, args))
-
-    def count(self):
-        n = 0
-        env = self
-        while env:
-            n += len(env)
-            env = env.outer
-        return n
-
-    def get(self, k):
-        env = self
-        while env:
-            if k in env:
-                return env[k]
-            env = env.outer
-        raise ValueError(k)
-
-    def keys1(self):
-        s = set()
-        env = self
-        while env:
-            s.update(env.keys())
-            env = env.outer
-        return s
-
-
 def isConcrete(a):
     match a:
         case float() | int() | str():
--- lambda/interpreter.py
+++ lambda/interpreter.py
@@ -3,6 +3,36 @@ import operator
 from etc import *
 
 
+class Env(dict):
+    def __init__(self, outer=None, params=(), args=()):
+        self.outer = outer
+        self.update(zip(params, args))
+
+    def count(self):
+        n = 0
+        env = self
+        while env:
+            n += len(env)
+            env = env.outer
+        return n
+
+    def get(self, k):
+        env = self
+        while env:
+            if k in env:
+                return env[k]
+            env = env.outer
+        raise ValueError(k)
+
+    def keys1(self):
+        s = set()
+        env = self
+        while env:
+            s.update(env.keys())
+            env = env.outer
+        return s
+
+
 class Closure:
     def __init__(self, env, params, body):
         self.env = env
@@ -12,6 +42,9 @@ class Closure:
     def __call__(self, *args):
         env = Env(self.env, self.params, args)
         return ev(env, self.body)
+
+    def __eq__(self, other):
+        raise TypeError()
 
 
 ops = (
