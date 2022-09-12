e309c0e5b0edb3c39170f416344662c26f4b2692
Russell Wallace <russell.wallace@gmail.com>
2022-09-12 14:52:30 +0100

    main

---
+++ code_gen2/build.bat
@@ -0,0 +1,1 @@
+cl /IC:\ml\code_gen2 C:\ml\code_gen2\olivine.cc
---
+++ code_gen2/olivine.cc
@@ -0,0 +1,32 @@
+#include <olivine.h>
+
+#include <new>
+
+#ifdef _WIN32
+#include <windows.h>
+
+namespace {
+LONG WINAPI handler(struct _EXCEPTION_POINTERS* ExceptionInfo) {
+	if (ExceptionInfo->ExceptionRecord->ExceptionCode == EXCEPTION_STACK_OVERFLOW)
+		WriteFile(GetStdHandle(STD_ERROR_HANDLE), "Stack overflow\n", 15, 0, 0);
+	else {
+		fprintf(stderr, "Exception code %lx\n", ExceptionInfo->ExceptionRecord->ExceptionCode);
+//		stackTrace();
+	}
+	ExitProcess(1);
+}
+} // namespace
+#else
+#include <unistd.h>
+#endif
+
+int main(int argc, const char** argv) {
+	std::set_new_handler([]() {
+		perror("new");
+		exit(1);
+	});
+#ifdef _WIN32
+	AddVectoredExceptionHandler(0, handler);
+#endif
+	return 0;
+}
