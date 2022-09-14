ad0d3346f62dc3261b268d54e924eded1be0fc92
Russell Wallace <russell.wallace@gmail.com>
2022-09-15 00:11:21 +0100

    list

---
+++ code_gen2/lo/list.cc
@@ -0,0 +1,21 @@
+#include <olivine.h>
+
+static list* mk(int n) {
+	auto r = (list*)xmalloc(offsetof(list, v) + n * sizeof(val*));
+	r->tag = t_list;
+	r->n = n;
+	return r;
+}
+
+list* mk(val* a) {
+	auto r = mk(1);
+	r->v[0] = a;
+	return r;
+}
+
+list* mk(val* a, val* b) {
+	auto r = mk(2);
+	r->v[0] = a;
+	r->v[1] = b;
+	return r;
+}
--- code_gen2/lo/list.h
+++ code_gen2/lo/list.h
@@ -2,3 +2,6 @@ struct list: val {
 	unsigned n;
 	val* v[];
 };
+
+list* mk(val* a);
+list* mk(val* a, val* b);
