e221cfe42622c2e0ab7c8839ef9e5d53181576e6
Russell Wallace <russell.wallace@gmail.com>
2022-09-14 08:08:34 +0100

    test

--- code_gen2/test.cc
+++ code_gen2/test.cc
@@ -1,6 +1,17 @@
 #include <olivine.h>
 
 void main() {
+	assert(isPow2(1));
+	assert(isPow2(2));
+	assert(isPow2(4));
+	assert(isPow2(8));
+	assert(!isPow2(3));
+	assert(!isPow2(5));
+	assert(!isPow2(6));
+	assert(!isPow2(7));
+	assert(!isPow2(9));
+	assert(!isPow2(10));
+
 	dyn a(1.0);
 	assert(a.tag() == d_num);
 
