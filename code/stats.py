53a609543bc8e2392c0df4507c3258a3d3b3e07d
Russell Wallace <russell.wallace@gmail.com>
2023-03-01 14:57:16 +0000

    scramble

--- code/stats.py
+++ code/stats.py
@@ -1,15 +1,26 @@
 import argparse
+import random
 
-import reader
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+import zz
 
 exts = set()
 exts.add(".java")
 
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-r", "--scramble", help="amount of scrambling", type=int, default=10
+)
+parser.add_argument("-s", "--seed", help="random number seed")
 parser.add_argument("-z", "--size", help="chunk size", type=int, default=100)
 parser.add_argument("src_dir")
 args = parser.parse_args()
 
-filename = reader.get_filenames(exts, args.src_dir)[0]
-print(reader.get_file(filename))
-print(args.size)
+if args.seed is not None:
+    random.seed(options.seed)
+
+v = zz.get_chunks(exts, args.src_dir, args.size)[0]
+print(v)
+print(zz.scramble(v, args.scramble))
--- code/zz.py
+++ code/zz.py
@@ -1,6 +1,16 @@
 import os
+import random
+
+import torch
 
 alphabet_size = 126 - 31 + 1
+
+
+def get_chunks(exts, src_dir, size):
+    r = []
+    for filename in get_filenames(exts, src_dir):
+        r.extend(chop(get_file(filename), size))
+    return r
 
 
 def get_filenames(exts, src_dir):
@@ -49,6 +59,28 @@ def chop(v, size):
     for i in range(0, len(v) - (size - 1), size):
         r.append(v[i : i + size])
     return r
+
+
+def one_hot(a):
+    v = [0.0] * alphabet_size
+    v[a] = 1.0
+    return v
+
+
+def tensor(v):
+    r = []
+    for a in v:
+        r.extend(one_hot(a))
+    return torch.as_tensor(r)
+
+
+def scramble(v, n):
+    v = v.copy()
+    for i in range(n):
+        j = random.randrange(len(v))
+        k = random.randrange(len(v))
+        v[j], v[k] = v[k], v[j]
+    return v
 
 
 # unit tests
