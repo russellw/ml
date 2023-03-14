ce6014a65df29c721b0d8d4829bd660247b8ef00
Russell Wallace <russell.wallace@gmail.com>
2023-03-14 04:10:35 +0000

    bloom

--- rl/test-bloom-1b7.py
+++ rl/test-bloom-1b7.py
@@ -1,5 +1,34 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+#https://towardsdatascience.com/getting-started-with-bloom-9e3295459b65
+import transformers
+from transformers import BloomForCausalLM
+from transformers import BloomTokenizerFast
+import torch
 
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b7")
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-1b7")
+tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-1b7")
 
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b7")
+prompt = "It was a dark and stormy night"
+result_length = 50
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Greedy Search
+print(tokenizer.decode(model.generate(inputs["input_ids"], 
+                       max_length=result_length
+                      )[0]))
+
+# Beam Search
+print(tokenizer.decode(model.generate(inputs["input_ids"],
+                       max_length=result_length, 
+                       num_beams=2, 
+                       no_repeat_ngram_size=2,
+                       early_stopping=True
+                      )[0]))
+
+# Sampling Top-k + Top-p
+print(tokenizer.decode(model.generate(inputs["input_ids"],
+                       max_length=result_length, 
+                       do_sample=True, 
+                       top_k=50, 
+                       top_p=0.9
+                      )[0]))
+                                                                  
