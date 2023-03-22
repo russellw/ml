449377c42b3ce4736f3ab23dbc995f42b5afd57b
Russell Wallace <russell.wallace@gmail.com>
2023-03-22 09:48:23 +0000

    fmt

--- sat/sudoku.py
+++ sat/sudoku.py
@@ -1,29 +1,36 @@
-#https://stackoverflow.com/questions/45471152/how-to-create-a-sudoku-puzzle-in-python
-base  = 3
-side  = base*base
+# https://stackoverflow.com/questions/45471152/how-to-create-a-sudoku-puzzle-in-python
+base = 3
+side = base * base
 
 # pattern for a baseline valid solution
-def pattern(r,c): return (base*(r%base)+r//base+c)%side
+def pattern(r, c):
+    return (base * (r % base) + r // base + c) % side
+
 
 # randomize rows, columns and numbers (of valid base pattern)
 from random import sample
-def shuffle(s): return sample(s,len(s)) 
-rBase = range(base) 
-rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
-cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
-nums  = shuffle(range(1,base*base+1))
+
+
+def shuffle(s):
+    return sample(s, len(s))
+
+
+rBase = range(base)
+rows = [g * base + r for g in shuffle(rBase) for r in shuffle(rBase)]
+cols = [g * base + c for g in shuffle(rBase) for c in shuffle(rBase)]
+nums = shuffle(range(1, base * base + 1))
 
 # produce board using randomized baseline pattern
-board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+board = [[nums[pattern(r, c)] for c in cols] for r in rows]
 
-for line in board: print(line)
+for line in board:
+    print(line)
 
-squares = side*side
-empties = squares * 3//4
-for p in sample(range(squares),empties):
-    board[p//side][p%side] = 0
+squares = side * side
+empties = squares * 3 // 4
+for p in sample(range(squares), empties):
+    board[p // side][p % side] = 0
 
 numSize = len(str(side))
 for line in board:
-    print(*(f"{n or '.':{numSize}} " for n in line)) 
-    
+    print(*(f"{n or '.':{numSize}} " for n in line))
