51995a8b2b43c0c4d8650f3bd57c5559753fdf3f
Russell Wallace <russell.wallace@gmail.com>
2023-03-24 12:51:53 +0000

    nodes

--- sat/interpreter.py
+++ sat/interpreter.py
@@ -23,18 +23,29 @@ ops = {
 
 
 def eva(a):
-    if isinstance(a, tuple):
-        f = eva(a[0])
-        return f(*a[1:])
-    if isinstance(a, str):
-        return ops[a]
-    return a
+    if a.op == "const":
+        return a.val
+    f = ops[a.op]
+    args = [eva(b) for b in a.args]
+    return f(*args)
 
 
 def test(a, b):
     assert eva(a) == b
 
 
+class Node:
+    def __init__(self, op, *args):
+        self.op = op
+        self.args = args
+
+
+def const(val):
+    a = Node("const")
+    a.val = val
+    return a
+
+
 if __name__ == "__main__":
-    test(1, 1)
-    test(("+", 1, 2), 3)
+    test(const(1), 1)
+    test(Node("+", const(1), const(2)), 3)
