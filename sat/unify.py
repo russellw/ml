5fdf63aa5b9be536850bf3f55a953f79f6cc450d
Russell Wallace <russell.wallace@gmail.com>
2023-03-24 14:31:18 +0000

    unify

--- sat/unify.py
+++ sat/unify.py
@@ -2,7 +2,7 @@ def is_var(a):
     return isinstance(a, str) and a.startswith("$")
 
 
-def occurs(d, a, b):
+def occurs(a,b,d):
     assert a.startswith("$")
     if a == b:
         return True
@@ -14,32 +14,32 @@ def occurs(d, a, b):
         return occurs(d, a, d[b])
 
 
-def unify_var(d, a, b):
+def unify_var(a,b,d):
     assert is_var(a)
     if a in d:
         return unify(d, d[a], b)
     if b in d:
         return unify(d, a, d[b])
-    if occurs(d, a, b):
+    if occurs(a,b,d):
         return
     d[a] = b
     return True
 
 
-def unify(d, a, b):
+def unify(a,b,d):
     # this version of unify skips the type check
     # because it makes no sense to ask the type of a type
     if a == b:
         return True
 
     if is_var(a):
-        return unify_var(d, a, b)
+        return unify_var(a,b,d)
     if is_var(b):
-        return unify_var(d, b, a)
+        return unify_var( b, a,d)
 
     if isinstance(a, tuple) and isinstance(b, tuple) and len(a) == len(b):
         for i in range(len(a)):
-            if not unify(d, a[i], b[i]):
+            if not unify( a[i], b[i],d):
                 return
         return True
 
@@ -68,91 +68,91 @@ if __name__ == "__main__":
 
     # Succeeds. (tautology)
     d = {}
-    assert unify(d, a, a)
+    assert unify( a, a,d)
     assert len(d) == 0
 
     # a and b do not match
     d = {}
-    assert not unify(d, a, b)
+    assert not unify(a,b,d)
 
     # Succeeds. (tautology)
     d = {}
-    assert unify(d, x, x)
+    assert unify( x, x,d)
     assert len(d) == 0
 
     # x is unified with the constant a
     d = {}
-    assert unify(d, a, x)
+    assert unify( a, x,d)
     assert len(d) == 1
     assert replace(x, d) == a
 
     # x and y are aliased
     d = {}
-    assert unify(d, x, y)
+    assert unify( x, y,d)
     assert len(d) == 1
     assert replace(x, d) == replace(y, d)
 
     # function and constant symbols match, x is unified with the constant b
     d = {}
-    assert unify(d, (f, a, x), (f, a, b))
+    assert unify( (f, a, x), (f, a, b),d)
     assert len(d) == 1
     assert replace(x, d) == b
 
     # f and g do not match
     d = {}
-    assert not unify(d, (f, a), (g, a))
+    assert not unify( (f, a), (g, a),d)
 
     # x and y are aliased
     d = {}
-    assert unify(d, (f, x), (f, y))
+    assert unify( (f, x), (f, y),d)
     assert len(d) == 1
     assert replace(x, d) == replace(y, d)
 
     # f and g do not match
     d = {}
-    assert not unify(d, (f, x), (g, y))
+    assert not unify( (f, x), (g, y),d)
 
     # Fails. The f function symbols have different arity
     d = {}
-    assert not unify(d, (f, x), (f, y, z))
+    assert not unify( (f, x), (f, y, z),d)
 
     # Unifies y with the term g(x)
     d = {}
-    assert unify(d, (f, (g, x)), (f, y))
+    assert unify( (f, (g, x)), (f, y),d)
     assert len(d) == 1
     assert replace(y, d) == (g, x)
 
     # Unifies x with constant a, and y with the term g(a)
     d = {}
-    assert unify(d, (f, (g, x), x), (f, y, a))
+    assert unify( (f, (g, x), x), (f, y, a),d)
     assert len(d) == 2
     assert replace(x, d) == a
     assert replace(y, d) == (g, a)
 
     # Returns false in first-order logic and many modern Prolog dialects (enforced by the occurs check).
     d = {}
-    assert not unify(d, x, (f, x))
+    assert not unify( x, (f, x),d)
 
     # Both x and y are unified with the constant a
     d = {}
-    assert unify(d, x, y)
-    assert unify(d, y, a)
+    assert unify( x, y,d)
+    assert unify( y, a,d)
     assert len(d) == 2
     assert replace(x, d) == a
     assert replace(y, d) == a
 
     # As above (order of equations in set doesn't matter)
     d = {}
-    assert unify(d, a, y)
-    assert unify(d, x, y)
+    assert unify( a, y,d)
+    assert unify( x, y,d)
     assert len(d) == 2
     assert replace(x, d) == a
     assert replace(y, d) == a
 
     # Fails. a and b do not match, so x can't be unified with both
     d = {}
-    assert unify(d, x, a)
+    assert unify( x, a,d)
     assert len(d) == 1
-    assert not unify(d, b, x)
+    assert not unify( b, x,d)
 
     print("ok")
