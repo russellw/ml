41777738a65f80e25adc54b88e1d238684625418
Russell Wallace <russell.wallace@gmail.com>
2023-03-29 15:16:07 +0100

    defs

--- sat/interpreter.py
+++ sat/interpreter.py
@@ -1,61 +1,69 @@
 import operator
 
 
-class Op:
-    def __init__(self, t, f):
+class Def:
+    def __init__(self, t, val):
         self.t = t
-        self.f = f
+        self.val = val
 
 
 # https://docs.python.org/3/library/operator.html
-ops = {
-    "*": Op(("num", "num", "num"), operator.mul),
-    "neg": Op(("num", "num"), operator.neg),
-    "+": Op(("num", "num", "num"), operator.add),
-    "-": Op(("num", "num", "num"), operator.sub),
-    "<": Op(("bool", "num", "num"), operator.lt),
-    "<=": Op(("bool", "num", "num"), operator.le),
-    "=": Op(("bool", "$t", "$t"), operator.eq),
-    "div": Op(("num", "num", "num"), operator.floordiv),
-    "mod": Op(("num", "num", "num"), operator.mod),
-    "pow": Op(("num", "num", "num"), operator.pow),
-    "at": Op(("$t", ("list", "$t"), "num"), lambda a, b: a[b]),
-    "cons": Op((("list", "$t"), "$t", ("list", "$t")), lambda a, b: (a,) + b),
-    "hd": Op(("$t", ("list", "$t")), lambda a: a[0]),
-    "len": Op(("num", ("list", "$t")), lambda a: len(a)),
-    # "map": Op((),lambda f, a: tuple(map(f, a))),
-    "and": Op(("bool", "bool", "bool"), None),
-    "or": Op(("bool", "bool", "bool"), None),
-    "if": Op(("$t", "bool", "$t", "$t"), None),
-    "not": Op(("bool", "bool"), operator.not_),
-    "tl": Op((("list", "$t"), ("list", "$t")), lambda a: a[1:]),
-    "/": Op(("num", "num", "num"), operator.truediv),
+defs = {
+    "0": Def("num", 0),
+    "1": Def("num", 1),
+    "false": Def("bool", False),
+    "true": Def("bool", True),
+    "*": Def(("num", "num", "num"), operator.mul),
+    "neg": Def(("num", "num"), operator.neg),
+    "+": Def(("num", "num", "num"), operator.add),
+    "-": Def(("num", "num", "num"), operator.sub),
+    "<": Def(("bool", "num", "num"), operator.lt),
+    "<=": Def(("bool", "num", "num"), operator.le),
+    "=": Def(("bool", "$t", "$t"), operator.eq),
+    "div": Def(("num", "num", "num"), operator.floordiv),
+    "mod": Def(("num", "num", "num"), operator.mod),
+    "pow": Def(("num", "num", "num"), operator.pow),
+    "at": Def(("$t", ("list", "$t"), "num"), lambda a, b: a[b]),
+    "cons": Def((("list", "$t"), "$t", ("list", "$t")), lambda a, b: (a,) + b),
+    "hd": Def(("$t", ("list", "$t")), lambda a: a[0]),
+    "len": Def(("num", ("list", "$t")), lambda a: len(a)),
+    # "map": Def((),lambda f, a: tuple(map(f, a))),
+    "and": Def(("bool", "bool", "bool"), None),
+    "or": Def(("bool", "bool", "bool"), None),
+    "if": Def(("$t", "bool", "$t", "$t"), None),
+    "not": Def(("bool", "bool"), operator.not_),
+    "tl": Def((("list", "$t"), ("list", "$t")), lambda a: a[1:]),
+    "/": Def(("num", "num", "num"), operator.truediv),
 }
 
 
 def ev(a, env):
     if isinstance(a, str):
         return env[a]
-    if isinstance(a, tuple) and a:
-        o = a[0]
-        if o == "and":
-            return ev(a[1], env) and ev(a[2], env)
-        if o == "if":
-            return ev(a[2], env) if ev(a[1], env) else ev(a[3], env)
-        if o == "or":
-            return ev(a[1], env) or ev(a[2], env)
-        f = ev(o, env)
-        args = [ev(b, env) for b in a[1:]]
-        return f(*args)
-    return a
+    assert isinstance(a, tuple) or isinstance(a, list)
+
+    if not a:
+        return ()
+    o = a[0]
+
+    if o == "and":
+        return ev(a[1], env) and ev(a[2], env)
+    if o == "if":
+        return ev(a[2], env) if ev(a[1], env) else ev(a[3], env)
+    if o == "or":
+        return ev(a[1], env) or ev(a[2], env)
+
+    f = ev(o, env)
+    args = [ev(b, env) for b in a[1:]]
+    return f(*args)
 
 
 def run(a):
     env = {}
-    for o in ops:
-        op = ops[o]
-        if op.f:
-            env[o] = op.f
+    for o in defs:
+        d = defs[o]
+        if d.val:
+            env[o] = d.val
     return ev(a, env)
 
 
@@ -64,6 +72,6 @@ def test(a, b):
 
 
 if __name__ == "__main__":
-    test(1, 1)
-    test(("+", 1, 2), 3)
+    test("1", 1)
+    test(("+", "1", "1"), 2)
     print("ok")
