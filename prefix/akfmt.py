88d6091443ce21a475e253ce74c9a9fc76bbf641
Russell Wallace <russell.wallace@gmail.com>
2023-04-02 22:37:46 +0100

    pprint

--- prefix/akfmt.py
+++ prefix/akfmt.py
@@ -4,6 +4,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("files", nargs="*")
 args = parser.parse_args()
+
 # tokenizer
 def constituent(c):
     if c.isspace():
@@ -67,23 +68,73 @@ def expr():
     return s
 
 
+# printer
+def tuple_depth(a):
+    if isinstance(a, tuple):
+        return max(map(tuple_depth, a), default=0) + 1
+    return 0
+
+
+def indent(n):
+    out.append("  " * n)
+
+
+def pprint1(a, dent):
+    if isinstance(a, tuple):
+        out.append("(")
+        if a:
+            out.append(a[0])
+            if tuple_depth(a) <= 5:
+                for b in a[1:]:
+                    out.append(" ")
+                    pprint1(b, 0)
+            else:
+                dent += 1
+                for b in a[1:]:
+                    out.append("\n")
+                    indent(dent)
+                    pprint1(b, dent)
+        out.append(")")
+        return
+    out.append(a)
+
+
+def pprint(a):
+    pprint1(a, 0)
+    out.append("\n")
+
+
+# top level
 def do(filename):
     global toks
     global text
     global ti
+    global out
+
+    # read
     toks = []
     ti = 0
     text = open(filename).read()
     lex()
 
+    # parse
     v = []
     while tok:
         v.append(expr())
-    print(v)
+
+    # print
+    out = []
+    for a in v:
+        pprint(a)
+    out = "".join(out)
+    print(out, end="")
 
 
 for f in args.files:
-    for root, dirs, files in os.walk(f):
-        for filename in files:
-            if os.path.splitext(filename)[1] == ".k":
-                do(os.path.join(root, filename))
+    if os.path.isdir(f):
+        for root, dirs, files in os.walk(f):
+            for filename in files:
+                if os.path.splitext(filename)[1] == ".k":
+                    do(os.path.join(root, filename))
+        continue
+    do(f)
