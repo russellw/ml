d9f2abe4bc37d4db05ca660923a9fb641de865e3
Russell Wallace <russell.wallace@gmail.com>
2023-04-10 16:44:41 +0100

    origin

--- life/animate.py
+++ life/animate.py
@@ -11,6 +11,8 @@ parser.add_argument(
 )
 parser.add_argument("-r", "--rand", help="random pattern", action="store_true")
 parser.add_argument("-s", "--seed", help="random number seed", type=int)
+parser.add_argument("-x", help="origin X coordinate", type=int, default=0)
+parser.add_argument("-y", help="origin Y coordinate", type=int, default=0)
 parser.add_argument("-z", "--size", help="grid size", type=int, default=256)
 parser.add_argument("file", nargs="?")
 args = parser.parse_args()
@@ -19,6 +21,10 @@ if args.seed is not None:
     random.seed(args.seed)
 
 size = args.size
+x0 = args.x
+y0 = args.y
+x1 = x0 + size
+y1 = y0 + size
 
 g = None
 if args.rand is not None:
@@ -29,11 +35,11 @@ if args.file:
 
 def update(frame):
     g.run()
-    img.set_data(g.get_data(0, 0, size, size))
+    img.set_data(g.get_data(x0, y0, x1, y1))
     return img
 
 
 fig, ax = plt.subplots()
-img = ax.imshow(g.get_data(0, 0, size, size), interpolation="nearest")
+img = ax.imshow(g.get_data(x0, y0, x1, y1), interpolation="nearest")
 ani = animation.FuncAnimation(fig, update, interval=0)
 plt.show()
